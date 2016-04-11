8bcab78be75943a897048430340c33c158af500d
Axel Pettersson <axpe@AD.SSIS.NU>
2016-04-11 22:45:48 +0200

    Sadface.py now work in BASH

--- sadface.py
+++ sadface.py
@@ -1,10 +1,13 @@
+from __future__ import print_function
 
 with open('sadface.txt') as f:
   while True:
     line = f.readline()
     if not line:
-      print "End of file"
+      print("End of file")
       break
-    newline = line.replace("b","\033[31;47m")
-    newline = newline.replace(":","\033[31;45m")
-    print newline
+    newline = line.replace("b","\033[39;49m ")
+    newline = newline.replace(":","\033[33;43m")
+    newline = newline.replace("w","\033[97;107m")
+    
+    print(newline, end="")
