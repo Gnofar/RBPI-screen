044f6339a3cd9aae9d9c542c5e6442e5e2e79d86
Flaxen <alcaj@AD.SSIS.NU>
2016-04-11 12:59:46 +0200

    index för sida

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<head>
+</head>
+
+<body>
+	
+	<div id="grund">
+		<div id="top">
+			<h1>Green Turtle</h1>
+		</div>
+		
+		<div id="knapp1">
+			<h3>Användare</h3>
+		</div>
+		
+		<div id="knapp2">
+			<h3>SoppHÄMTARE</h3>
+		</div>
+		
+		<div id="knapp3">
+			<h3>Utvecklare</h3>
+		</div>
+		
+		<div id="siffra">
+			<h5>Ditt område</h5>
+			<div id="nummer">
+				246
+			</div>
+		</div>
+		
+		<div id="karta">
+			placeholder
+		</div>
+		
+		<div id="område">
+			Väsby ^ 1337
+			Väsby ^ 1337
+			Väsby ^ 1337
+			Väsby ^ 1337
+		</div>
+		
+		<div id="footer">
+			copyright c Barbros Änglar
+		</div>
+			
+			
+		
+	</div>
+	
+	
+	
+	
+</body>
